3ac58848afbac1fb7adf419183241e88604edb7f
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-01-13 13:28:57 -0700

    Renamed package.

--- src/extension.ts
+++ src/extension.ts
@@ -15,11 +15,13 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "liaradb.helloWorld",
+    "liaradb-explorer.helloWorld",
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from LiaraDB!");
+      vscode.window.showInformationMessage(
+        "Hello World from liaradb-explorer!"
+      );
     }
   );
 
